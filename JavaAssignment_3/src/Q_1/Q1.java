a01a52755c543c7a267001816e3c9f1ac8aeb2ac
Inderpreet <inder17375@gmail.com>
2023-03-03 00:40:11 +0530

    till asgn3 ques 2

---
+++ JavaAssignment_3/src/Q_1/Q1.java
@@ -0,0 +1,16 @@
+package Q_1;
+//Write a program to display values of enums using a constructor & getPrice() method
+// (Example display house & their prices)
+public class Q1 {
+    public static void main(String[] args){
+
+          //  Enums are trated as objects(By default are objects)
+        // their constructor are private
+        // enumms are public static final
+        //
+        for(enumClass obj : enumClass.values()){
+            System.out.println("Rate For :" + obj + "is in Rs " + obj.getPrice());
+        }
+
+    }
+}
---
+++ JavaAssignment_3/src/Q_2/Q2.java
@@ -0,0 +1,55 @@
+package Q_2;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+//) Create a User class with fields:  firstname, lastname, age, phonenumber.
+// Write a program which accepts values of user fields from commandline,
+// create object and append that to a text file.
+// After every user creation the program should prompt: "Do you want to continue creating users?
+// (Type QUIT to exit)" and keep on accepting values and writing to file unitl user quits.
+public class Q2 {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        Scanner scn = new Scanner(System.in) ;
+
+        File file = new File("test.txt");
+
+         ;
+        // File Writer can take 2 parameters 1st is File argument 2nd is true which append true or false
+
+        while(true){
+//            System.out.println("Enter Id ");
+//            int id = scn.nextInt() ;
+
+            System.out.println("Enter the Details");
+
+            System.out.println("Enter the FirstName");
+            String F_name = scn.nextLine() ;
+            System.out.println("Enter the LastName") ;
+            String L_name = scn.nextLine() ;
+
+            System.out.println("Enter the Age") ;
+            int age = scn.nextInt() ;
+
+            System.out.println("Enter The Phone Number");
+            int phone = scn.nextInt() ;
+
+            singleTclass s1 = singleTclass.getInstance(F_name,L_name ,age , phone) ;
+            System.out.println(s1.showDetails());
+
+            try(BufferedWriter br = new BufferedWriter(new FileWriter(file,true))){ //
+                br.write(s1.showDetails());
+                br.newLine();
+
+
+            }catch (IOException e){
+                System.out.println("Unable To Read File");
+            }
+        }
+
+    }
+}
