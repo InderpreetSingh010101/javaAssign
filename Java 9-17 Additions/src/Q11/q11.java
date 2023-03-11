2ff704ad4e0ac5f10b6999f1f3e102db4e96fdc9
Inderpreet <inder17375@gmail.com>
2023-03-11 17:56:39 +0530

    Q11 Done

--- Java 9-17 Additions/src/Q11/q11.java
+++ Java 9-17 Additions/src/Q11/q11.java
@@ -5,5 +5,22 @@ package Q11;
 public class q11 {
     public static void main(String[] args) {
         System.out.println("Q11");
+
+        recordStudent stu = new recordStudent("Ips" , 1234 , 22) ;
+        System.out.println(stu  + "No Of Obj created " + recordStudent.count);
+        recordStudent stu1 = new recordStudent("Ips" , 1234 , 22) ;
+        System.out.println(stu  + "No Of Obj created " + recordStudent.count);
+
+        System.out.println("------------------------------------------------------------");
+
+        System.out.println("Equals Method ");
+        System.out.println(stu.equals(stu1));
+
+        System.out.println("HashCodes");
+        System.out.println(stu.hashCode());
+        System.out.println(stu1.hashCode());
+
+
+
     }
 }
